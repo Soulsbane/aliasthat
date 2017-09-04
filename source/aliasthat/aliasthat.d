d8d95f4e1604a8b097a57643d09a6e749b74ad54
Paul Crane <paul@acheronsoft.com>
2017-09-03 21:00:12 -0500

    Add tests for allMembers and nameOf.

--- source/aliasthat/aliasthat.d
+++ source/aliasthat/aliasthat.d
@@ -1,5 +1,10 @@
 module aliasthat.aliasthat;
 
+version(unittest)
+{
+	import fluent.asserts;
+}
+
 alias Helper(alias T) = T;
 
 private template allMembers(T)
@@ -7,7 +12,27 @@ private template allMembers(T)
 	enum allMembers = __traits(allMembers, T);
 }
 
+unittest
+{
+	struct Test
+	{
+		string name;
+		uint id;
+		bool enabled;
+	}
+
+	allMembers!Test.length.should.equal(3);
+	allMembers!Test[0].should.equal("name");
+	[allMembers!Test].should.equal(["name", "id", "enabled"]);
+}
+
 template nameOf(alias nameType)
 {
 	enum string nameOf = __traits(identifier, nameType);
+}
+
+unittest
+{
+	int name;
+	assert(nameOf!name == "name");
 }
